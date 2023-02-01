2b7a768ebe67dd6a95967e443e3b40427660d13b
Amirhanna2001 <amirhanna111200@gmail.com>
2023-02-01 15:34:12 +0200

    Add project files.

---
+++ EmployeeManagement.Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public string PhotoPath { get; set; }
+        
+        public Gender Gender { get; set; }
+        public Department Department { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
